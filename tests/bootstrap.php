f103e05467eb6bbec306176f66cbdba6461e5a9e
Micah Wood <micah@wpscholar.com>
2025-05-25 13:11:57 -0400

    Update bootstrap.php file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,19 +8,11 @@
 // Load the Composer autoloader.
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// Load the plugin.
-require_once dirname( __DIR__ ) . '/RandomPostOnRefresh.php';
-
 // Load WordPress test suite functions
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
-/**
- * Load the plugin.
- */
-function _load_random_post_on_refresh_plugin() {
-	require dirname( __DIR__ ) . '/RandomPostOnRefresh.php';
-}
-tests_add_filter( 'muplugins_loaded', '_load_random_post_on_refresh_plugin' );
-
 // Start up the WP testing environment
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+
+// Load the plugin.
+require_once dirname( __DIR__ ) . '/RandomPostOnRefresh.php';
